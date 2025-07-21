131040f364a33f8d0f193463416317f6dda44b98
ashok8501 <kuchipudiashokreddy15@gmail.com>
2025-07-21 11:32:23 +0530

    Update Contact.jsx

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
           <p className=' font-semibold text-lg text-gray-600'>OUR OFFICE</p>
           <p className=' text-gray-500'>Vizag <br />Kommadhi</p>
           <p className=' text-gray-500'>Tel: (415) 555-0132 <br /> Email: kuchipudiashokreddy15@gmail.com</p>
-          <p className=' font-semibold text-lg text-gray-600'>CAREERS AT PRESCRIPTO</p>
+          <p className=' font-semibold text-lg text-gray-600'>CAREERS AT SwiftCare</p>
           <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
         </div>
