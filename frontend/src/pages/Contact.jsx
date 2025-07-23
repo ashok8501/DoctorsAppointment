174c65415296dc9b3e546cc561aeb6f15342f67a
Ashok <kuchipudiashokreddy@gmail.com>
2025-07-23 11:56:03 +0530

    ✅ Resolved merge conflict in Contact.jsx

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -4,7 +4,6 @@ import { assets } from '../assets/assets'
 const Contact = () => {
   return (
     <div>
-
       <div className='text-center text-2xl pt-10 text-[#707070]'>
         <p>CONTACT <span className='text-gray-700 font-semibold'>US</span></p>
       </div>
@@ -12,20 +11,17 @@ const Contact = () => {
       <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm'>
         <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
         <div className='flex flex-col justify-center items-start gap-6'>
-          <p className=' font-semibold text-lg text-gray-600'>OUR OFFICE</p>
-<<<<<<< HEAD
-          <p className=' text-gray-500'>54709 Willms Station <br /> Suite 350, Washington, USA</p>
-          <p className=' text-gray-500'>Tel: (415) 555-0132 <br /> Email: greatstackdev@gmail.com</p>
-=======
-          <p className=' text-gray-500'>Vizag <br />Kommadhi</p>
-          <p className=' text-gray-500'>Tel: (415) 555-0132 <br /> Email: kuchipudiashokreddy15@gmail.com</p>
->>>>>>> 394a389043590822cdf28a02f9f1934ee90dd9bb
-          <p className=' font-semibold text-lg text-gray-600'>CAREERS AT SwiftCare</p>
-          <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
-          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+          <p className='font-semibold text-lg text-gray-600'>OUR OFFICE</p>
+          <p className='text-gray-500'>Vizag <br />Kommadhi</p>
+          <p className='text-gray-500'>Tel: (415) 555-0132 <br /> Email: kuchipudiashokreddy15@gmail.com</p>
+
+          <p className='font-semibold text-lg text-gray-600'>CAREERS AT SwiftCare</p>
+          <p className='text-gray-500'>Learn more about our teams and job openings.</p>
+          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>
+            Explore Jobs
+          </button>
         </div>
       </div>
-
     </div>
   )
 }
